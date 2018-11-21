460791a8e14e3b67b86446c01c5247030612f56b
devans1913 <dwynn.evans@gmail.com>
2018-11-21 11:14:01 -0600

    updates connection.js

--- config/connection.js
+++ config/connection.js
@@ -5,24 +5,17 @@ var connection;
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
-  
   connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
     password: 'VeriShip$18',
     database: "burgers_db"
-  })
-}
+  });
+};
 
 // Secure connection
-connection.connect(function (err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+connection.connect (); 
 
 // Export connection 
 module.exports = connection;
